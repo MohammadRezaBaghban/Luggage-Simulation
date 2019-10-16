a3aabc55622dfa361f553e18d21058bc553e89b8
Kotai,Himanshu H <h.kotai@student.fontys.nl>
2019-10-16 20:44:30 +0200

    Editnodeclass

--- Simulation/Rail Bag Simulation/Rail Bag Simulation/Node.cs
+++ Simulation/Rail Bag Simulation/Rail Bag Simulation/Node.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Rail_Bag_Simulation
 {
-    class Node
-    {
+      class Node
+        {
+            private object obj; //the item it refers to
+            private Node next;    // the next node it refers to; null if there does not exist a next node
+
+            public Node(object obj)
+            {
+                this.obj = obj;
+                this.next = null;
+            }
+
+            public object getItem() { return this.obj; }
+
+            public Node getNextNode() { return this.next; }
+            public void setNext(Node n) { this.next = n; }
+
+        }
     }
-}
+
