c40c1eb78ca5b20abde2090562f1a42b9de66893
Shema,Gael M.G <g.shema@student.fontys.nl>
2019-10-19 16:06:03 +0200

    Finished The bag class with its respective static methods

--- Simulation/Rail Bag Simulation/Rail Bag Simulation/Bag.cs
+++ Simulation/Rail Bag Simulation/Rail Bag Simulation/Bag.cs
@@ -12,7 +12,7 @@ namespace Rail_Bag_Simulation
         private static int _idToGive = 100;
         private string _lastSeenLocation;
         private int _id;
-        private SuspiciousBagtype _suspicious;
+        private SuspiciousBagtype? _suspicious;
         private float _weight;
         private Destination _destination;
         private string _terminalAndGate;
@@ -22,6 +22,15 @@ namespace Rail_Bag_Simulation
         public Bag(SuspiciousBagtype suspicious, float weight, Destination destination, string terminalAndGate)
         {
             this._suspicious = suspicious;
+            this._weight = weight;
+            this._id = ++_idToGive;
+            this._destination = destination;
+            this._terminalAndGate = terminalAndGate;
+        }
+
+        public Bag(float weight, Destination destination, string terminalAndGate)
+        {
+            this._suspicious = null;
             this._weight = weight;
             this._id = ++_idToGive;
             this._destination = destination;
@@ -41,23 +50,14 @@ namespace Rail_Bag_Simulation
         private static int GetTheSuspiciousBagDistr(int totalNrOfBags, int totalNrOfSuspBags)
         {
 
-            var ResultToTest = Convert.ToDouble((totalNrOfBags - totalNrOfSuspBags) / totalNrOfSuspBags);
-            var FirstDigitAfterDot = ResultToTest - (int) ResultToTest;
-                FirstDigitAfterDot = Math.Round(FirstDigitAfterDot, 1, MidpointRounding.AwayFromZero);
-                if (FirstDigitAfterDot < 0.5)
-                {
-                    return Convert.ToInt32(ResultToTest);
-                }
-                else
-                {
-                    return Convert.ToInt32(ResultToTest) - 1;
-                }
+            var ResultToTest = Convert.ToInt32((decimal)(totalNrOfBags) / (decimal)totalNrOfSuspBags);
+            return ResultToTest;
         }
 
         public static List<Bag> GenerateBag(int nbrOfBags, int nbrOfBagsDrugs, int nbrOfBagsWeapons, int nbrOfBagsFlammable, int nbrBagsOthers)
         {
             var bags = new List<Bag>();
-            
+
 
             int totalNrOfSuspicious = nbrOfBagsDrugs + nbrOfBagsWeapons + nbrOfBagsFlammable + nbrBagsOthers;
 
@@ -67,17 +67,25 @@ namespace Rail_Bag_Simulation
             }
 
             var theSuspiciousBagDistr = GetTheSuspiciousBagDistr(nbrOfBags, totalNrOfSuspicious);
+            var suspiciousbags =
+                GenerateSuspiciousBags(nbrOfBagsDrugs, nbrOfBagsWeapons, nbrOfBagsFlammable, nbrBagsOthers);
 
 
-            for(int i=0; i <= nbrOfBags; i++)
+            for (int i = 0; i <= nbrOfBags; i++)
             {
                 //for every suspicious bag to be created, it will be placed after the distribution value.
                 if (i % theSuspiciousBagDistr == 0)
                 {
-                    
+                    bags.Add(suspiciousbags[0]);
+                    suspiciousbags.RemoveAt(0);
+                    continue;
                 }
+
+                bags.Add(new Bag(random.Next(10, 22), (Destination) random.Next(1, 12),
+                    "T" + random.Next(1, 3) + "G" + random.Next(1, 3)));
+
             }
-            return null;
+            return bags;
         }
 
         private static List<Bag> GenerateSuspiciousBags(int nbrOfBagsDrugs, int nbrOfBagsWeapons, int nbrOfBagsFlammable,
@@ -89,12 +97,36 @@ namespace Rail_Bag_Simulation
             {
                 if (nbrOfBagsDrugs > 0)
                 {
-                    templist.Add(new Bag(SuspiciousBagtype.Drug, random.Next(10,22), (Destination)random.Next(1,12), "T"+ random.Next(1,3)+"G"+random.Next(1,3)));
+                    templist.Add(new Bag(SuspiciousBagtype.Drug, random.Next(10, 22), (Destination)random.Next(1, 12), "T" + random.Next(1, 3) + "G" + random.Next(1, 3)));
                     nbrOfBagsDrugs--;
+                    totalnumber--;
+                }
+
+                if (nbrOfBagsWeapons > 0)
+                {
+                    templist.Add(new Bag(SuspiciousBagtype.Weapons, random.Next(10, 22), (Destination)random.Next(1, 12), "T" + random.Next(1, 3) + "G" + random.Next(1, 3)));
+                    nbrOfBagsWeapons--;
+                    totalnumber--;
+
+                }
+
+                if (nbrOfBagsFlammable > 0)
+                {
+                    templist.Add(new Bag(SuspiciousBagtype.Flammables, random.Next(10, 22), (Destination)random.Next(1, 12), "T" + random.Next(1, 3) + "G" + random.Next(1, 3)));
+                    nbrOfBagsFlammable--;
+                    totalnumber--;
+
+                }
+
+                if (nbrBagsOthers > 0)
+                {
+                    templist.Add(new Bag(SuspiciousBagtype.Other, random.Next(10, 22), (Destination)random.Next(1, 12), "T" + random.Next(1, 3) + "G" + random.Next(1, 3)));
+                    nbrBagsOthers--;
+                    totalnumber--;
                 }
             }
 
-            return null;
+            return templist;
         }
     }
 }
