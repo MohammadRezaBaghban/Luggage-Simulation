49a609f375dd601e27b0e7bb8bc5e7b5c7669bc9
Cheperkova,Nadya N <n.cheperkova@student.fontys.nl>
2019-10-27 11:18:34 +0100

    changes

---
+++ Simulation/Rail Bag Simulation/Rail Bag Simulation/SecurityNode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rail_Bag_Simulation
+{
+    class SecurityNode : Node
+    {
+        public override string Nodeinfo()
+        {
+            return "Security: \n";
+        }
+
+        public void ScanBagSecurity(Bag b)
+        {
+            Thread.Sleep(DelayTime);
+
+            if (b?.GetSecurityStatus() == null)
+            {
+                if (Next is ConveyorNode node)
+                {       
+                    node.Conveyor.PushBagToConveyorBelt(b);
+                }
+                return;
+            }
+            
+            Airport.Storage.StoreSuspiciousBag(b);
+        }
+    }
+}
