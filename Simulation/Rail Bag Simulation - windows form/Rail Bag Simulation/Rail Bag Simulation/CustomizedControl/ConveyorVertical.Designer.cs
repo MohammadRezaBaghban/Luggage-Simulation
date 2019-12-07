b52a840e6902cc571e0cfae487b0aba75f109f2f
dparpulov <dd.parpulov@gmail.com>
2019-12-07 14:45:30 +0100

    Vertical custom controller - conveyor finished

---
+++ Simulation/Rail Bag Simulation - windows form/Rail Bag Simulation/Rail Bag Simulation/CustomizedControl/ConveyorVertical.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Rail_Bag_Simulation.CustomizedControl
+{
+    partial class ConveyorVertical
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConveyorVertical));
+            this.slot2Vert = new System.Windows.Forms.PictureBox();
+            this.slot1Vert = new System.Windows.Forms.PictureBox();
+            this.slot3Vert = new System.Windows.Forms.PictureBox();
+            this.slot4Vert = new System.Windows.Forms.PictureBox();
+            this.slot5Vert = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.slot2Vert)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot1Vert)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot3Vert)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot4Vert)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot5Vert)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // slot2Vert
+            // 
+            this.slot2Vert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.slot2Vert.Image = ((System.Drawing.Image)(resources.GetObject("slot2Vert.Image")));
+            this.slot2Vert.Location = new System.Drawing.Point(0, 89);
+            this.slot2Vert.Name = "slot2Vert";
+            this.slot2Vert.Size = new System.Drawing.Size(104, 93);
+            this.slot2Vert.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.slot2Vert.TabIndex = 0;
+            this.slot2Vert.TabStop = false;
+            // 
+            // slot1Vert
+            // 
+            this.slot1Vert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.slot1Vert.Image = ((System.Drawing.Image)(resources.GetObject("slot1Vert.Image")));
+            this.slot1Vert.Location = new System.Drawing.Point(0, 0);
+            this.slot1Vert.Name = "slot1Vert";
+            this.slot1Vert.Size = new System.Drawing.Size(104, 93);
+            this.slot1Vert.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.slot1Vert.TabIndex = 1;
+            this.slot1Vert.TabStop = false;
+            // 
+            // slot3Vert
+            // 
+            this.slot3Vert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.slot3Vert.Image = ((System.Drawing.Image)(resources.GetObject("slot3Vert.Image")));
+            this.slot3Vert.Location = new System.Drawing.Point(0, 179);
+            this.slot3Vert.Name = "slot3Vert";
+            this.slot3Vert.Size = new System.Drawing.Size(104, 93);
+            this.slot3Vert.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.slot3Vert.TabIndex = 2;
+            this.slot3Vert.TabStop = false;
+            // 
+            // slot4Vert
+            // 
+            this.slot4Vert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.slot4Vert.Image = ((System.Drawing.Image)(resources.GetObject("slot4Vert.Image")));
+            this.slot4Vert.Location = new System.Drawing.Point(0, 269);
+            this.slot4Vert.Name = "slot4Vert";
+            this.slot4Vert.Size = new System.Drawing.Size(104, 93);
+            this.slot4Vert.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.slot4Vert.TabIndex = 3;
+            this.slot4Vert.TabStop = false;
+            // 
+            // slot5Vert
+            // 
+            this.slot5Vert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.slot5Vert.Image = ((System.Drawing.Image)(resources.GetObject("slot5Vert.Image")));
+            this.slot5Vert.Location = new System.Drawing.Point(0, 356);
+            this.slot5Vert.Name = "slot5Vert";
+            this.slot5Vert.Size = new System.Drawing.Size(104, 93);
+            this.slot5Vert.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.slot5Vert.TabIndex = 4;
+            this.slot5Vert.TabStop = false;
+            // 
+            // ConveyorVertical
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.slot5Vert);
+            this.Controls.Add(this.slot4Vert);
+            this.Controls.Add(this.slot3Vert);
+            this.Controls.Add(this.slot1Vert);
+            this.Controls.Add(this.slot2Vert);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "ConveyorVertical";
+            this.Size = new System.Drawing.Size(103, 447);
+            ((System.ComponentModel.ISupportInitialize)(this.slot2Vert)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot1Vert)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot3Vert)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot4Vert)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot5Vert)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox slot2Vert;
+        private System.Windows.Forms.PictureBox slot1Vert;
+        private System.Windows.Forms.PictureBox slot3Vert;
+        private System.Windows.Forms.PictureBox slot4Vert;
+        private System.Windows.Forms.PictureBox slot5Vert;
+    }
+}
