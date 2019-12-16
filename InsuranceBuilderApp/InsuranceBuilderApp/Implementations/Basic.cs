c3d38a88899e288af86478cc1933edd6e7f4d07a
400736 <400736@student.fontys.nl>
2019-12-16 14:52:10 +0100

    First commit with some stuff done!!

---
+++ InsuranceBuilderApp/InsuranceBuilderApp/Implementations/Basic.cs
@@ -0,0 +1,32 @@
+﻿using InsuranceBuilderApp.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceBuilderApp.Implementations
+{
+    public class Basic : IInsurancePackage
+    {
+        public decimal Cost()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal Deductable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Upgradable()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InsuranceBuilderApp/InsuranceBuilderApp/Interface/IInsurancePackage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceBuilderApp.Interface
+{
+    interface IInsurancePackage
+    {
+        string Name();
+        decimal Cost();
+        decimal Deductable();
+        bool Upgradable();
+    }
+}
